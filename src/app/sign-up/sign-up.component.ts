bf16d9d4e9548ed6b07d153650034da119606143
Elodie <elodie.dargot@gmail.com>
2023-09-27 16:00:46 +0200

    first commit

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent {
+
+  model: User = new User("","","","")
+
+  onSubmit(): void {
+    console.log(this.model)
+  }
+}
